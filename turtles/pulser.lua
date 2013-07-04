2c9ecb5ba25edb82c48de015d48483d95e6b76a6
David <admin@dav1d.de>
2013-07-04 18:07:49 +0200

    added redstone pulser

---
+++ turtles/pulser.lua
@@ -0,0 +1,21 @@
+local tArgs = { ... }
+
+function main()
+    local side = tArgs[1]
+    local sleep = tonumber(tArgs[2])
+    local inb = tonumber(tArgs[3])
+    inb = inb or 0.1
+
+    if not side and not sleep then
+        term.write("pulser side sleep")
+    end
+
+    while true do
+        redstone.setOutput(side, true)
+        os.sleep(inb)
+        redstone.setOutput(side, false)
+        os.sleep(sleep)
+    end
+end
+
+main()
